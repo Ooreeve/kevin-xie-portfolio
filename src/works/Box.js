3e7753630903ae330c1df35c068af153bb01d60c
Kevin <oorkin18.gmail.com>
2023-03-09 20:14:55 +0800

    works

---
+++ src/works/Box.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function Box(props) {
+    const box_styles = {
+        right: `${props.right}px`,
+        animation: props.ani,
+    };
+    const handleClick = () => {
+        window.open(props.url);
+    };
+    return (
+        <div className="box" style={box_styles} onClick={handleClick}>
+            <div className="left">
+                <img
+                    src={require("../images/home/home_bg_1_tiny.png")}
+                    alt="leaves"
+                    style={{ left: `${props.left}px`, top: `${props.top}px` }}
+                />
+                <img
+                    src={require("../images/home/home_bg_1_tiny.png")}
+                    alt="leaves"
+                    style={{ left: `${props.left}px`, top: `${props.top}px` }}
+                />
+            </div>
+            <div className="left_text">
+                <div className="no">{props.no}.</div>
+                <div className="box_title">{props.title}</div>
+            </div>
+            <div className="desc">{props.desc}</div>
+            <div className="right">
+                <img
+                    src={require(`../images/works/${props.img}.jpg`)}
+                    alt="website_img_sample"
+                />
+            </div>
+        </div>
+    );
+}
--- src/works/Works.js
+++ src/works/Works.js
@@ -1,106 +1,190 @@
 import { React, useEffect, useState, useContext, useRef } from "react";
 import { MyContext } from "../App";
+import Box from "./Box";
 
 export default function Works() {
     const [first, setFirst] = useContext(MyContext).first;
-    const [thumbTop, setThumbTop] = useState(0);
-    const [boxStyles, setBoxStyles] = useState([{}, {}, {}, {}, {}]);
-    const [thumbStyles, setThumbStyles] = useState({
-        transform: "",
-        height: "",
+    const [scrollPosn, setScrollPosn] = useState(0);
+    const [scrollSignStyles, setScrollSignStyles] = useState({ top: 100 });
+    const [arrowStyles, setArrowStyles] = useState({
+        top: { opacity: 0, height: 0 },
+        down: { opacity: 1, height: 17 },
+    });
+    const [boxLeftImgRanNum, setBoxLeftImgRanNum] = useState([]);
+    const [aniStyles, setAniStyles] = useState({
+        h1: {},
+        scroll_sign: {},
+        box_container: {},
+        box: [],
     });
 
-    const boxContainerRef = useRef(null);
+    //create_boxes------------------------------------
+    const box_data = [
+        {
+            title: "kuon yagi portfolio clone",
+            desc: "The website is a clone of an award-winning personal site designed by Kuon Yagi, which received Honorable Mention on Awwwards.The contents of the site have been replaced with Hunter x Hunter manga materials.",
+            img: "kuon_yagi_portfolio_clone",
+            url: "https://test-react-kuon.vercel.app/",
+        },
+        {
+            title: "the craftsmen clone",
+            desc: "This website is a clone of a company portal owned by The Craftsmen, a web development company. It was awarded Site of the Day by Awwwards on May 27, 2022. The contents of the site have been replaced with Star Wars materials.",
+            img: "the_craftsmen_clone",
+            url: "https://test-sass-thecraftsmen-kztnn6nyv-ooreeve.vercel.app/",
+        },
+        {
+            title: "gravity fall introduction website",
+            desc: "This website is an introduction site for a well known cartoon Gravity Falls. The design is simple and straightforward without too many embellishments, and it is written using basic html, css and vanilla JavaScript.",
+            img: "gravity_fall_introduction_website",
+            url: "https://test-vanilla-js-gravity-falls.vercel.app/index.html",
+        },
+        {
+            title: "none",
+            desc: "ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas maecenas pharetra",
+            img: "website_img_sample",
+            url: "https://www.youtube.com/",
+        },
+        {
+            title: "none",
+            desc: "ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas maecenas pharetra",
+            img: "website_img_sample",
+            url: "https://www.youtube.com/",
+        },
+    ];
 
-    useEffect(() => {
-        if (boxContainerRef.current) {
-            const boxContainer = boxContainerRef.current;
-
-            //set_thumb_height
-            const thumbHeight =
-                (boxContainer.clientHeight / boxContainer.scrollHeight) *
-                    boxContainer.clientHeight -
-                2;
-
-            setThumbStyles((prev) => ({
-                ...prev,
-                height: `${thumbHeight}px`,
-            }));
-
-            //set_box_posn
-            const box_elements_sttyles = [0, 1, 2, 3, 4].map((i) => {
-                const box_posn =
-                    boxContainer.childNodes[i].getBoundingClientRect().top;
+    //animation_function---------------------------
+    const setAni = () => {
+        const box_ani = Array(box_data.length)
+            .fill()
+            .map((item, index) => {
                 return {
-                    transform: `translateX(${-box_posn * 0.3 + 30}px)`,
+                    animation: `works_box 2.5s ${0.5 + index * 0.3}s forwards`,
                 };
             });
 
-            setBoxStyles(box_elements_sttyles);
+        setAniStyles({
+            h1: { animation: "works_h1 2.5s forwards" },
+            scroll_sign: { animation: "works_scroll_sign 1s 2.5s forwards" },
+            box_container: {
+                animation: "works_box_container 0.1s 2.5s forwards",
+            },
+            box: box_ani,
+        });
+    };
+
+    useEffect(() => {
+        //set_box_left_img_styles_random_number
+        const left_and_top = Array(box_data.length)
+            .fill()
+            .map(() => {
+                return {
+                    left: Math.floor(-600 * Math.random()),
+                    top: Math.floor(-200 * Math.random()),
+                };
+            });
+
+        setBoxLeftImgRanNum(left_and_top);
+
+        //set_in_animation
+        if (first == true) {
+            setTimeout(() => {
+                setAni();
+            }, 1500);
+            setFirst(false);
+        } else {
+            setTimeout(() => {
+                setAni();
+            }, 500);
         }
     }, []);
 
-    function handleScroll(e) {
-        // console.log([
-        //     e.target.scrollTop, //thumb positino
-        //     e.target.scrollHeight, //content height
-        //     e.target.clientHeight, //visibel area height
-        // ]);
-
-        //set_thumb_position
-        const thumbPosn = e.target.scrollTop / 2;
-
-        setThumbStyles((prev) => ({
-            ...prev,
-            transform: `translateY(${thumbPosn}px)`,
-        }));
-        setThumbTop(e.target.scrollTop);
-
-        //change_box_Y_position
-        const box_elements_styles = [0, 1, 2, 3, 4].map((i) => {
-            const box_posn = e.target.childNodes[i].getBoundingClientRect().top;
-
-            return {
-                // transform: `translateX(${-box_posn * 0.3}px)`,
-                transform: "skew(-16deg)",
-                left: `${-box_posn * 0.3}px`,
-            };
-        });
-
-        setBoxStyles(box_elements_styles);
-    }
-
-    //create_box_elements
-    const box_array = [0, 1, 2, 3, 4];
-    const box_elements = box_array.map((i) => {
+    const box_elements = box_data.map((item, index) => {
         return (
-            <div key={i} className="box" style={boxStyles[i]}>
-                {i}
-            </div>
+            boxLeftImgRanNum.length > 0 &&
+            aniStyles.box.length > 0 && (
+                <Box
+                    key={index}
+                    no={index + 1}
+                    title={item.title}
+                    desc={item.desc}
+                    img={item.img}
+                    right={150 + index * 100 - scrollPosn / 2.25}
+                    url={item.url}
+                    left={boxLeftImgRanNum[index].left}
+                    top={boxLeftImgRanNum[index].top}
+                    ani={aniStyles.box[index].animation}
+                />
+            )
         );
     });
 
-    const test = (e) => {
-        console.log(e.currentTarget.scrollTop);
-        e.currentTarget.scrollTop += 100;
-    };
+    //scroll_function---------------------------
+    function handle_scroll(e) {
+        const scroll_posn = e.target.scrollTop;
+        //box_position
+        setScrollPosn(scroll_posn);
+
+        //scroll_sign_top & arrow
+        if (scrollPosn > 30) {
+            setArrowStyles((prev) => {
+                return { ...prev, top: { height: 17, opacity: 1 } };
+            });
+            setScrollSignStyles((prev) => {
+                return { ...prev, top: 83 };
+            });
+        } else {
+            setArrowStyles((prev) => {
+                return { ...prev, top: { height: 0, opacity: 0 } };
+            });
+            setScrollSignStyles((prev) => {
+                return { ...prev, top: 100 };
+            });
+        }
+
+        if (scrollPosn > 58 + (box_data.length - 3) * 223) {
+            setArrowStyles((prev) => {
+                return { ...prev, down: { height: 0, opacity: 0 } };
+            });
+        } else {
+            setArrowStyles((prev) => {
+                return { ...prev, down: { height: 17, opacity: 1 } };
+            });
+        }
+    }
 
     return (
         <div className="works">
-            <div className="box_container_wrapper">
+            <div
+                className="scroll_sign"
+                style={{
+                    top: `${scrollSignStyles.top}px`,
+                    animation: aniStyles.scroll_sign.animation,
+                }}
+            >
                 <div
-                    className="box_container"
-                    ref={boxContainerRef}
-                    onScroll={handleScroll}
-                    onClick={test}
-                >
-                    {box_elements}
-                </div>
+                    className="up"
+                    style={{
+                        opacity: `${arrowStyles.top.opacity}`,
+                        height: `${arrowStyles.top.height}px`,
+                    }}
+                ></div>
+                <div className="bar"></div>
+                <div
+                    className="down"
+                    style={{
+                        opacity: `${arrowStyles.down.opacity}`,
+                        height: `${arrowStyles.down.height}px`,
+                    }}
+                ></div>
             </div>
-            <div className="scroll_bar">
-                <div className="thumb" style={thumbStyles}></div>
+            <div
+                className="box_container"
+                onScroll={handle_scroll}
+                style={aniStyles.box_container}
+            >
+                {box_elements}
             </div>
-            <h1>
+            <h1 style={aniStyles.h1}>
                 My<br></br>Works
             </h1>
         </div>
